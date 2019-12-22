36cb6638a7b9bbb2b62e666744c2977a1db7a641
liusikair <250740270@qq.com>
2019-12-23 05:59:11 +0800

    预处理翻车

--- 开波.bat
+++ 开波.bat
@@ -1,5 +1,23 @@
-@echo off
+﻿@echo off
 pushd %~dp0
+
+set "CodeData=%appdata%\Code\"
+set "BACK=%appdata%\Code\Backups"
+set "INFO=%USERPROFILE%\storage.json"
+set "FILE=%~dp0.vscode\storage.json"
+
+rem 预处理
+rd /S /Q "%BACK%"
+mklink /D "%BACK%" "%~dp0.vscode\Backups"
+
+rem 创建备份 防止硬链接被错误删除
+copy %FILE% %FILE%.back
+del /Q "%INFO%"
+
+mklink /H "%INFO%" %FILE%.back
+
+
+pause
 code  .
 exit
 

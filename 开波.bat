12a8152065ee3e8605055d8d8ded139b08e6306c
liusikair <250740270@qq.com>
2019-12-30 04:48:05 +0800

    取消软连接

--- 开波.bat
+++ 开波.bat
@@ -15,13 +15,13 @@ set "STORAGE1=%~dp0.vscode\storage.json"
 set "KEYSET1=%~dp0.vscode\keybindings.json"
 
 :: 清除其他程序的未保存数据的软连接
-rd /S /Q "%BACK0%"
-del /F /Q "%STORAGE0%"
+REM rd /S /Q "%BACK0%"
+REM del /F /Q "%STORAGE0%"
 del /F /Q "%KEYSET0%"
 
 :: 软连接
-mklink /D "%BACK0%" "%BACK1%"
-mklink /H "%STORAGE0%" "%STORAGE1%"
+REM mklink /D "%BACK0%" "%BACK1%"
+REM mklink /H "%STORAGE0%" "%STORAGE1%"
 mklink /H "%KEYSET0%" "%KEYSET1%"
 
 code  .
